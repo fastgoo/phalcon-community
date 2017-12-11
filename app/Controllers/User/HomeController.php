a778c5f0cc39c5fa3f40a0d28a2da2570db29b7d
yanwenwu <82664165@qq.com>
2017-12-11 21:06:00 +0800

    添加用户详情页面

---
+++ app/Controllers/User/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers\User;
+
+use App\Controllers\BaseController;
+
+class HomeController extends BaseController
+{
+    public function detailAction($id)
+    {
+          //查询用户的信息
+          //查询用户最近的发表的帖子
+          //查询用户最新的回答
+    }
+}
