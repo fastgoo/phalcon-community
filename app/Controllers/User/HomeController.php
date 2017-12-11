009cf026728e5fda97b89d680083e888a6f14cec
yanwenwu <82664165@qq.com>
2017-12-11 21:32:10 +0800

    添加查询

--- app/Controllers/User/HomeController.php
+++ app/Controllers/User/HomeController.php
@@ -3,11 +3,13 @@
 namespace App\Controllers\User;
 
 use App\Controllers\BaseController;
+use App\Models\ForumUser;
 
 class HomeController extends BaseController
 {
     public function detailAction($id)
     {
+          var_dump(ForumUser::findFirst($id)->toArray());die;
           //查询用户的信息
           //查询用户最近的发表的帖子
           //查询用户最新的回答
